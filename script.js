e94cae4ebb45928f8f223271b4cbccb4b919dbc7
LilDie <neobad.reyes@gmail.com>
2024-01-12 16:57:17 -0300

    hola

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-console.log ("Hola carlos")
-
-console.log ("Hola Diego")
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  genNew.addEventListener("click", setBg);
+  setBg()
